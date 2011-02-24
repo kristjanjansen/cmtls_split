03fecae114dbe88abc39540497e7f192131681f1
Kristjan Jansen <kristjan.jansen@gmail.com>
2011-02-24 13:41:04 +0200

    clean after

--- cmtls_split.sh
+++ cmtls_split.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 cd ~/Sites/github
-sudo rm -R Community-Tools
-sudo rm -R cmtls
-sudo rm -R cmtls_theme
-sudo rm -R cmtls_profile
 
 git clone https://github.com/communitytools/Community-Tools.git
 git clone --no-hardlinks Community-Tools cmtls
@@ -41,3 +37,7 @@ git remote add origin git@github.com:kristjanjansen/cmtls_profile.git
 git push origin master --force
 cd ..
 
+sudo rm -R Community-Tools
+sudo rm -R cmtls
+sudo rm -R cmtls_theme
+sudo rm -R cmtls_profile
